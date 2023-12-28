ebe66e08619e4b32641a67ab2707235618993b68
Anonymous-Shelby <70262420+Anonymous-Shelby@users.noreply.github.com>
2023-12-28 03:44:30 +0330

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,90 +1,69 @@
 import os
 import requests
-import time
-from telegram import Update
-from telegram.ext import Updater, CommandHandler, MessageHandler, Filters, CallbackContext
+from telegram import Bot, InputFile
+from telegram.ext import Updater, MessageHandler, Filters
+from tqdm import tqdm
 
-# Replace 'YOUR_BOT_TOKEN' with your actual bot token
-TOKEN = '5032426994:AAFTz0n2jDGKRAubthsGbCO1u3A01UNydKQ'
+# توکن ربات تلگرام خود را در اینجا قرار دهید
+TELEGRAM_BOT_TOKEN = '5032426994:AAFTz0n2jDGKRAubthsGbCO1u3A01UNydKQ'
 
-def start(update: Update, context: CallbackContext) -> None:
-    update.message.reply_text('سلام! برای دانلود فایل، لطفاً لینک مستقیم فایل را ارسال کنید.')
-
-def download_file(update: Update, context: CallbackContext) -> None:
-    url = update.message.text
-    chat_id = update.message.chat_id
+def download_and_upload_video(update, context):
+    # دریافت لینک از پیام
+    video_link = update.message.text
 
     try:
-        response = requests.get(url, stream=True)
-        total_size = int(response.headers.get('content-length', 0))
-        downloaded_size = 0
-        start_time = time.time()
+        # دانلود فایل ویدیو
+        video_file = download_video(video_link, update)
 
-        # Send initial download message
-        message = context.bot.send_message(chat_id, 'در حال دانلود...')
-        context.user_data['message_id'] = message.message_id
+        # آپلود فایل به تلگرام
+        upload_video_to_telegram(update, context, video_file)
 
-        with open('downloaded_file', 'wb') as file:
-            for chunk in response.iter_content(chunk_size=1024):
-                if chunk:
-                    file.write(chunk)
-                    downloaded_size += len(chunk)
-                    elapsed_time = time.time() - start_time
-
-                    # Calculate download speed
-                    download_speed = downloaded_size / (1024 * 1024) / elapsed_time  # in MB/s
-
-                    percentage = (downloaded_size / total_size) * 100
-                    remaining_size = total_size - downloaded_size
-
-                    # Update the existing message with download progress and speed
-                    new_content = f'در حال دانلود... {int(percentage)}%\n' \
-                                  f'حجم دانلود شده: {downloaded_size / (1024 * 1024):.2f} MB\n' \
-                                  f'حجم باقی‌مانده: {remaining_size / (1024 * 1024):.2f} MB\n' \
-                                  f'سرعت دانلود: {download_speed:.2f} MB/s'
-
-                    if not context.user_data.get('last_content') or context.user_data['last_content'] != new_content:
-                        # Update the existing message with download progress and speed
-                        context.bot.edit_message_text(
-                            chat_id=chat_id,
-                            message_id=context.user_data['message_id'],
-                            text=new_content
-                        )
-                        context.user_data['last_content'] = new_content
-                    else:
-                        # If content hasn't changed, wait for a short delay
-                        time.sleep(1)
-
-        # Upload the downloaded file to Telegram
-        with open('downloaded_file', 'rb') as file:
-            context.bot.send_document(chat_id, document=file)
-
-        # Send completion message
-        context.bot.edit_message_text(
-            chat_id=chat_id,
-            message_id=context.user_data['message_id'],
-            text='دانلود کامل شد!'
-        )
+        # حذف فایل موقت
+        os.remove(video_file)
 
     except Exception as e:
-        context.bot.edit_message_text(
-            chat_id=chat_id,
-            message_id=context.user_data['message_id'],
-            text=f'خطا: {e}'
-        )
+        print(f"Error: {e}")
+        update.message.reply_text("مشکلی در دانلود یا آپلود فایل وجود دارد.")
 
-    finally:
-        os.remove('downloaded_file')
+def download_video(video_link, update):
+    # ایجاد یک نام فایل موقت
+    temp_file_name = "temp_video.mp4"
 
-def main() -> None:
-    updater = Updater(TOKEN)
-    dp = updater.dispatcher
+    # دریافت اندازه فایل
+    response = requests.head(video_link)
+    file_size = int(response.headers.get('content-length', 0))
 
-    dp.add_handler(CommandHandler("start", start))
-    dp.add_handler(MessageHandler(Filters.text & ~Filters.command, download_file))
+    # نمایش نوار پیشرفت
+    with tqdm(total=file_size, unit='B', unit_scale=True, unit_divisor=1024, desc='Downloading') as progress_bar:
+        # دانلود فایل ویدیو
+        response = requests.get(video_link, stream=True)
+        with open(temp_file_name, 'wb') as video_file:
+            for chunk in response.iter_content(chunk_size=8192):
+                video_file.write(chunk)
+                progress_bar.update(len(chunk))
 
+    update.message.reply_text("دانلود فایل با موفقیت انجام شد.")
+    return temp_file_name
+
+def upload_video_to_telegram(update, context, video_file):
+    # دریافت chat_id
+    chat_id = update.message.chat_id
+
+    # ارسال فایل به تلگرام
+    context.bot.send_video(chat_id=chat_id, video=InputFile(video_file))
+
+def main():
+    # ساخت یک ربات تلگرام
+    bot = Bot(token=TELEGRAM_BOT_TOKEN)
+    updater = Updater(token=TELEGRAM_BOT_TOKEN, use_context=True)
+
+    # ایجاد یک MessageHandler برای پاسخ به پیام‌های متنی کاربران
+    message_handler = MessageHandler(Filters.text & ~Filters.command, download_and_upload_video)
+    updater.dispatcher.add_handler(message_handler)
+
+    # شروع ربات
     updater.start_polling()
     updater.idle()
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     main()
